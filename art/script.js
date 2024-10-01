2882760e8a06b4c48aa6bb1650289a8d6e151cae
0wlish <tortugavpn@gmail.com>
2024-10-01 14:42:24 -0400

    js futzing

--- art/script.js
+++ art/script.js
@@ -1,16 +1,16 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src.substring(0, this.src.indexOf("_LR")) + this.src.substring(this.src.indexOf("_LR")+3);
-  modalImg.src = modalImg.src.replace("LowRes", "images");
-  captionText.innerHTML = this.alt;
+function openModal(){
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById("myImg");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    //img.onclick = openModal();
+    modal.style.display = "block";
+    modalImg.src = this.src.substring(0, this.src.indexOf("_LR")) + this.src.substring(this.src.indexOf("_LR")+3);
+    modalImg.src = modalImg.src.replace("LowRes", "images");
+    captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
