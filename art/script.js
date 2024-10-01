5dec936d634da169da027dedfc95b84122ee3b6d
0wlish <tortugavpn@gmail.com>
2024-10-01 10:13:54 -0400

    fixed issue with modal not popping up

--- art/script.js
+++ art/script.js
@@ -1,11 +1,11 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
+
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-console.log(captionText); //this is null and it should not be
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
