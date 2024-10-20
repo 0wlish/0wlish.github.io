8345f02490484bda85e4190260c880556636c17a
0wlish <tortugavpn@gmail.com>
2024-10-20 11:19:59 -0400

    slideshow now moves forwards but not backwards

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Get the modal
-var modal = document.getElementById("myModal");
-const slides = document.getElementsByClassName("slides");
-var modalImg = document.getElementById("modalImage");
-var currentSlide;
+var modal = document.getElementById("myModal"); //the entire modal
+const slides = document.getElementsByClassName("slides"); //all images to go in slideshow
+var modalImg = document.getElementById("modalImage"); //current modal image
+var currentSlide; 
 
+//replace sources of slides with high-res images
 for (let i = 0; i < slides.length; i++) {
   slides.item(i).src = slides.item(i).src.substring(0, slides.item(i).src.indexOf("_LR")) + slides.item(i).src.substring(slides.item(i).src.indexOf("_LR")+3);
   slides.item(i).src = slides.item(i).src.replace("LowRes", "images");
@@ -23,10 +24,11 @@ function openModal(){
 */
 
 function showSlides(n) {
-  modalImg.src = slides.item(n).src;
+  //opens modal to specific slide number
+  modalImg.src = slides.item(n).src; //modal image source equals source of slides at n
   openModal();
-  currentSlide = n;
-  console.log(currentSlide);
+  currentSlide = n; //remember the current slide number
+  //console.log(currentSlide);
 
   /*
   if (n > slides.length) {
@@ -47,9 +49,10 @@ function showSlides(n) {
 }
 
 function plusSlides(n) {
-  modalImg.src.innerHTML = slides.item(currentSlide + n).src;
-  console.log(currentSlide + n);
-  console.log(slides.item(currentSlide + n).src);
+  currentSlide += n; //current slide is itself plus n
+  modalImg.src = slides.item(currentSlide).src; //updates source of modal image to new source
+  console.log(currentSlide);
+  console.log(slides.item(currentSlide).src);
 }
 
 function openModal() {
